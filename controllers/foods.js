d673e4176ceb1e80347084365a5fd22ddd7831d4
Ashley Laisure <ashley.laisure@gmail.com>
2025-02-12 19:22:36 -0500

    delete route added

--- controllers/foods.js
+++ controllers/foods.js
@@ -25,17 +25,17 @@ router.get('/new', (req, res) => {
 });
 
 // Delete
-// router.delete('/:pantryId', async (req, res) => {
-//     try {
-//         const currentUser = await User.findById(req.session.user._id);
-//         currentUser.pantry.id(req.params.pantryId).deleteOne();
-//         await currentUser.save();
-//         res.redirect(`/users/${currentUser._id}/foods`);
-//     } catch (error) {
-//         console.log(error);
-//         res.redirect('/')
-//     }
-// })
+router.delete('/:pantryId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.pantry.id(req.params.pantryId).deleteOne();
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/foods`);
+    } catch (error) {
+        console.log(error);
+        res.redirect('/')
+    }
+})
 // Update
 // Create
 router.post('/', async (req, res) => {
